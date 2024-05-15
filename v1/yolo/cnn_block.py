88b886a0abb28c361febdb34eeb9ecd0b5eafe97
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-15 20:18:40 +0530

    updated cnn block

--- v1/yolo/cnn_block.py
+++ v1/yolo/cnn_block.py
@@ -2,13 +2,14 @@ import tensorflow as tf
 import keras
 
 
-class CNNBlock(tf.Module):
+class CNNBlock(keras.layers.Layer):
     def __init__(self,  out_channels, **kwargs):
         super(CNNBlock, self).__init__()
         self.conv = keras.layers.Conv2D(
-            filters=out_channels, use_bias=False, padding='same', **kwargs)
+            filters=out_channels, use_bias=False, **kwargs)
         self.batchnorm = keras.layers.BatchNormalization()
-        self.leakyrelu = keras.activations.leaky_relu(negative_slope=0.1)
+        self.leakyrelu = keras.layers.LeakyReLU(negative_slope=0.1)
 
-    def __call__(self, x):
-        return self.leakyrelu(self.batchnorm(self.conv(x)))
+    def call(self, x):
+        x = self.conv(x)
+        return self.leakyrelu(self.batchnorm(x))
