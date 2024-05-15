5a291c059b2fd4af6f55f5e9b03ed4afd890f7c6
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-15 20:20:25 +0530

    updated model included padding and did some refractoring

--- v1/yolo/model.py
+++ v1/yolo/model.py
@@ -1,37 +1,37 @@
 import tensorflow as tf
 import keras
-from v1.utils import constants
-from cnn_block import CNNBlock
-
+from v1.utils import architecture
+from v1.yolo.cnn_block import CNNBlock
+import numpy as np
 
 
 class Yolov1(tf.Module):
     def __init__(self, in_channels=3, **kwargs):
         super(Yolov1, self).__init__()
-        self.architecture = constants.architecture
-        self.in_channels = in_channels,
+        self.architecture = architecture
+        self.in_channels = in_channels
         self.darknet = self.create_conv_layers(self.architecture)
         self.fcl = self.create_fc_layers(**kwargs)
 
     def __call__(self, x):
         x = self.darknet(x)
-        return self.fcl(keras.layers.Flatten(x))
+        x = keras.layers.Flatten()(x)
+        return self.fcl(x)
 
     def create_conv_layers(self, architecture):
-        layers = []
-        in_channels = self.in_channels
-
+        layers = keras.models.Sequential()
         for x in architecture:
             if type(x) == tuple:
-                layers += [
+                layers.add(keras.layers.ZeroPadding2D(x[3]))
+                layers.add(
                     CNNBlock(
                         out_channels=x[1],
                         kernel_size=x[0],
                         strides=x[2]
                     )
-                ]
+                )
             elif type(x) == str:
-                layers += [keras.layers.MaxPool2D(pool_size=2, strides=2)]
+                layers.add(keras.layers.MaxPool2D(pool_size=2, strides=2))
 
             elif type(x) == list:
                 conv1 = x[0]
@@ -39,22 +39,24 @@ class Yolov1(tf.Module):
                 num_repeats = x[2]
 
                 for _ in range(num_repeats):
-                    layers += [
+                    layers.add(keras.layers.ZeroPadding2D(x[3]))
+                    layers.add(
                         CNNBlock(
                             conv1[1],
                             kernel_size=conv1[0],
                             strides=conv1[2]
                         )
-                    ]
-                    layers += [
+                    )
+                    layers.add(keras.layers.ZeroPadding2D(x[3]))
+                    layers.add(
                         CNNBlock(
                             conv2[1],
                             kernel_size=conv2[0],
                             strides=conv2[2]
                         )
-                    ]
+                    )
 
-        return keras.models.Sequential(*layers)
+        return layers
 
     def create_fc_layers(self, split_size, num_boxes, num_classes):
         S, B, C = split_size, num_boxes, num_classes
@@ -63,6 +65,6 @@ class Yolov1(tf.Module):
             keras.layers.Flatten(),
             keras.layers.Dense(496),  # original paper usee 4960 units
             keras.layers.Dropout(0),
-            keras.activations.leaky_relu(negative_slope=0.1),
+            keras.layers.LeakyReLU(negative_slope=0.1),
             keras.layers.Dense(S*S*(C+B*S))
         ])
