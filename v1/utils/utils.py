a51b3bb48463873258a1cd4ee743c1bf8c7d5fc9
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-19 15:50:55 +0530

    added iou returning function

--- v1/utils/utils.py
+++ v1/utils/utils.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+
+
 class AssertionFailedError(Exception):
     pass
 
@@ -5,3 +8,38 @@ class AssertionFailedError(Exception):
 def custom_assert(condition, message="Assertion failed"):
     if not condition:
         raise AssertionFailedError(message)
+
+
+def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
+    if box_format == "midpoint":
+        box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+        box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+        box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
+        box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
+        box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+        box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+        box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
+        box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
+
+    if box_format == "corners":
+        box1_x1 = boxes_preds[..., 0:1]
+        box1_y1 = boxes_preds[..., 1:2]
+        box1_x2 = boxes_preds[..., 2:3]
+        box1_y2 = boxes_preds[..., 3:4]  # (N, 1)
+        box2_x1 = boxes_labels[..., 0:1]
+        box2_y1 = boxes_labels[..., 1:2]
+        box2_x2 = boxes_labels[..., 2:3]
+        box2_y2 = boxes_labels[..., 3:4]
+
+    x1 = tf.maximum(box1_x1, box2_x1)
+    y1 = tf.maximum(box1_y1, box2_y1)
+    x2 = tf.minimum(box1_x2, box2_x2)
+    y2 = tf.minimum(box1_y2, box2_y2)
+
+    # getting max is for the case when they do not intersect
+    intersection = tf.maximum((x2 - x1), 0) * tf.maximum((y2 - y1), 0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
