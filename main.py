226fbec1ef12380c121d2b407e40f298dbbe623b
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-20 13:11:22 +0530

    update main

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from v1 import testRunner
+import keras
+from v1 import testRunner, Yolov1, YoloLoss, train_fn, hyperparameters, miscellaneous
 
 if __name__ == "__main__":
     testRunner()
+    model = YoloLoss(split_size=7, num_boxes=2, num_classes=20)
+    optimizer = keras.optimizers.Adam(
+        learning_rate=hyperparameters["LEARNING_RATE"]
+    )
+    loss_fn = YoloLoss(split_size=7, num_boxes=2, num_classes=20)
+
+    for epoch in range(hyperparameters["EPOCHS"]):
+        print(f"EPOCH{epoch}")
